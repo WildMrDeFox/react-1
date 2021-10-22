b28442a51244bc79d19b9b83585c5eb7c2701d24
WildMrDeFox <wildmrdefox01@gmail.com>
2021-10-22 16:32:28 +0700

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header className='header'>
+            <img src="https://cdn.pixabay.com/photo/2018/03/27/15/05/logo-3266214_1280.png" alt=""/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+function Profile(){
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://i.redd.it/bohmf1gew2y61.jpg" alt=""/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                    <div>post 3</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
